070b93550e769104a16b23d8c200a1c9777f78a4
ptakenouchi <36687765+ptakenouchi@users.noreply.github.com>
2019-10-23 00:21:39 -0400

    all

---
+++ Query_Diseases VS Fast Food_Final.sql
@@ -0,0 +1,37 @@
+CREATE TABLE FastFood (
+   Keys varchar  NOT NULL ,
+   Province varchar  NOT NULL ,
+   City varchar  NOT NULL ,
+   Brand varchar  NOT NULL ,
+   PRIMARY KEY (
+       Keys
+   )
+);
+
+CREATE TABLE 2017+Diseases (
+   dma varchar  NOT NULL ,
+   2017+Diarrhea int  NOT NULL ,
+   2017+Obesity int  NOT NULL ,
+   2017+Diabetes int  NOT NULL ,
+   2017+Cancer int  NOT NULL ,
+   2017+Depression int  NOT NULL
+);
+CREATE TABLE 2016+Diseases (
+   dma varchar  NOT NULL ,
+   2016+Diarrhea int  NOT NULL ,
+   2016+Obesity int  NOT NULL ,
+   2016+Diabetes int  NOT NULL ,
+   2016+Cancer int  NOT NULL ,
+   2016+Depression int  NOT NULL
+);
+CREATE TABLE 2015+Diseases (
+   dma varchar  NOT NULL ,
+   2015+Diarrhea int  NOT NULL ,
+   2015+Obesity int  NOT NULL ,
+   2015+Diabetes int  NOT NULL ,
+   2015+Cancer int  NOT NULL ,
+   2015+Depression int  NOT NULL
+
+TRUNCATE TABLE FastFood 
+
+	
